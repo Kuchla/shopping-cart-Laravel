c0d4ac2428a594832872e2b154f9a9a3d331aec4
Kuchla <josni_kuchla@hotmail.com>
2019-03-21 11:49:59 -0300

    Added info about the purpose of the getAddToCart function

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -12,11 +12,14 @@ class CartController extends Controller
     public function getAddToCart(Request $request, $id)
     {
         $product = Product::find($id);
+        // $oldCart receives session cart if already exists
         $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        // create a new cart, passing the old data if exist a old cart
         $cart = new Cart($oldCart);
         $cart->add($product);
+        // Update cart session
         $request->session()->put('cart', $cart);
-        
+
         dd($request->session()->get('cart'));
     }
 }
